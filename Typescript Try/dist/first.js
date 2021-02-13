5aa7236fe0abd2dd4f22645e6a33a05d24296014
RianIslam <rianislamdr@gmail.com>
2021-02-13 21:46:24 +0600

    arrow

--- Typescript Try/dist/first.js
+++ Typescript Try/dist/first.js
@@ -1,4 +1,24 @@
 "use strict";
-let money = 50;
-money = 29;
-money = 0;
+let good;
+let club = 'real madrid';
+console.log(club);
+good = 44;
+let Famous = false;
+let famous;
+good = 39;
+//function type
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(3, 5);
+add('addm', 'git');
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+const user = fullName("mrtui", "githum");
+function doubleItAndConsole(num) {
+    const result = num * 3;
+    console.log(result);
+}
+const output = doubleItAndConsole(7);
+console.log('output ' + output);
--- Typescript Try/first.ts
+++ Typescript Try/first.ts
@@ -10,7 +10,7 @@ good =39;
 //function type
 
 function add(num1:number | string ,num2:number|string){
-    return num1+ num2;
+    return (num1 + num2);
 }
 add(3,5);
 add('addm','git')
@@ -22,3 +22,16 @@ function fullName (firstName:string,lastName:string):string{
 }
 
 const user =  fullName("mrtui","githum")
+
+
+function doubleItAndConsole(num:number){
+    const result = num * 3;
+    console.log(result);
+}
+const output =  doubleItAndConsole(7);
+console.log('output '+output)
+
+
+//arrow
+
+const multiply = (x:number,)
