e5739221175fb3ee0716d95b3747375cea256ec2
RianIslam <rianislamdr@gmail.com>
2021-02-13 21:15:39 +0600

    function type

--- Typescript Try/first.ts
+++ Typescript Try/first.ts
@@ -1,10 +1,24 @@
 let good:number
 let club:string = 'real madrid';
-
 console.log(club)
 good = 44;
 let Famous:boolean = false;
 let famous:boolean;
-
-
 good =39;
+
+
+//function type
+
+function add(num1:number | string ,num2:number|string){
+    return num1+ num2;
+}
+add(3,5);
+add('addm','git')
+
+
+
+function fullName (firstName:string,lastName:string):string{
+    return firstName + " " +lastName;
+}
+
+const user =  fullName("mrtui","githum")
