617b3f0889bec12034edaa71a4e14eca3ba4e00f
mdrianislam0or1 <rianislamdr@gmail.com>
2024-04-10 23:59:41 +0600

    add more

--- glacialglam-backend/src/app.ts
+++ glacialglam-backend/src/app.ts
@@ -1,28 +1,28 @@
-import { Application } from "express";
-import express, { Request, Response } from "express";
-import cors from "cors";
-import errorHandler from "./app/middleware/errorHandler";
-import router from "./app/routes";
+import { Application } from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import errorHandler from './app/middleware/errorHandler';
+import router from './app/routes';
 
 const app: Application = express();
-
-
 
 app.use(express.json());
 // Routes
 
-app.use(cors({ 
-  origin: 'http://localhost:5173', // Update this with the actual origin of your frontend
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: 'https://glacial-glam.web.app', // Update this with the actual origin of your frontend
+    credentials: true,
+  }),
+);
 
-app.use("/", router);
+app.use('/', router);
 
 // Default route
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
-    status: "success",
-    message: "Welcome to ProductExplorerHub API",
+    status: 'success',
+    message: 'Welcome to ProductExplorerHub API',
   });
 });
 
